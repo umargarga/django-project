407803a8987abd740f823f90d9ebe7d533d9ce82
Umar Yusuf Dayyabu <udayyabuyusuf@edu.salford.ac.uk>
2021-07-09 13:26:00 +0100

    Worked on navigation bar

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from challenges import views
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="index"),
     path("<int:month>", views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name="month-challenge"),
 ]
